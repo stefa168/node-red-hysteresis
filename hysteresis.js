4bce3dac742a70b0c58e81c02fdb5ebc044c1e43
Stefano Vittorio Porta <stefa168@hotmail.it>
2023-12-16 17:52:34 +0100

    refactor(hysteresis): refactored code to be more maintainable

--- hysteresis.js
+++ hysteresis.js
@@ -1,46 +1,48 @@
 module.exports = function (RED) {
 
 	function HysteresisNode(config) {
+		/* Setup */
 		RED.nodes.createNode(this, config);
 
 		let node = this;
 		let nodeContext = this.context();
 
-		// this.OutRisingType = config.OutRisingType;
 		node.ThresholdRising = config.ThresholdRising;
-		// this.OutFallingType = config.OutFallingType;
 		node.ThresholdFalling = config.ThresholdFalling;
-
-		// Should we also use the first message received, or only begin to work after the second one?
-		node.initialMessage = config.InitialMessage;
-
-		this.property = config.PayloadProperty ?? 'payload';
-
 		// Parse the set values for the thresholds
 		const ThresholdRising = Number.parseFloat(node.ThresholdRising);
 		const ThresholdFalling = Number.parseFloat(node.ThresholdFalling);
 
 		if (Number.isNaN(ThresholdRising) || Number.isNaN(ThresholdFalling)) {
-			node.status({fill: 'red', shape: 'ring', text: 'Thresholds missing'});
-			node.error(new Error('One or more thresholds are not set or are not numbers.'));
+			node.status({fill: 'red', shape: 'ring', text: "Thresholds missing"});
+			node.error("One or more thresholds are not set or are not numbers.");
 		}
 
+		// Should we also use the first message received, or only begin to work after the second one?
+		node.initialMessage = config.InitialMessage;
+		this.property = config.PayloadProperty ?? 'payload';
+
+		/* Execution */
 		// Retrieve the last value from the context
 		node.direction = nodeContext.get('direction') ?? undefined;
 
-		// Set initial status
-		// if (!ThresholdRising) {
-		// 	node.status({fill: 'red', shape: 'ring', text: 'Thresholds missing'});
-		// } else {
 		node.status({fill: 'yellow', shape: 'ring', text: ThresholdFalling + '/--/' + ThresholdRising});
-		// }
 
 		node.on('input', function (msg, send, done) {
 			send = send || function () {
 				node.send.apply(node, arguments);
 			};
 
-			// Get the current value from the message
+			done = done || function (err) {
+				if (err) {
+					node.error(err, msg);
+				}
+			};
+
+			// Get the current value from the message.
+			// Since the return value is 'any', we need to parse it to a number.
+			// If the message property does not exist, we will get `undefined`, and `Number(undefined)` returns `NaN`.
+			// Because of this, we need to be sure that we got a number.
 			// https://nodered.org/docs/api/modules/v/1.3/@node-red_util_util.html#.getMessageProperty
 			const currentValue = Number(RED.util.getMessageProperty(msg, node.property));
 
@@ -48,133 +50,109 @@ module.exports = function (RED) {
 			if (Number.isNaN(currentValue)) {
 				node.status({fill: 'red', shape: 'ring', text: 'Payload is not a number'});
 				const err = new Error('Payload is not a number');
-				if (done) {
-					// Node-RED 1.0 compatible
-					done(err);
-				} else {
-					// Node-RED 0.x compatible
-					node.error(err, msg);
-				}
+				done(err);
 			}
 
 			const newMessage = RED.util.cloneMessage(msg);
 
 			let newDirection = undefined;
 
+			function setStatus(color, shape, text) {
+				node.status({
+					fill: color,
+					shape: shape,
+					text: `${ThresholdFalling}/${currentValue}/${ThresholdRising} (${text})`
+				});
+			}
+
+			/**
+			 * This section handles the hysteresis logic.<br>
+			 * It checks the current value against the last value, and the thresholds, and determines if the direction
+			 * of the system.<br>
+			 * If the direction changes, it will send a message with the new direction, and update the context.
+			 */
 			// If the direction is not set, we are in the initial state.
 			// If we are told to use the initial message, we will use it to set the direction.
 			if (node.initialMessage && node.direction === undefined) {
-
-				// Check if the current value is above the rising threshold; if so, we are in the high band, and we
-				// should send the received message
 				if (currentValue >= ThresholdRising) {
+					// Check if the current value is above the rising threshold; if so, we are in the high band, and we
+					// should send the received message
 					newMessage.hysteresis_direction = 'initial high';
-					send(newMessage);
-
 					newDirection = 'high';
 
-					node.status({
-						fill: 'green',
-						shape: 'dot',
-						text: `${ThresholdFalling}/${currentValue}/${ThresholdRising} (initial high band)`
-					});
-
+					send(newMessage);
+					setStatus('green', 'dot', 'initial high band');
+				} else if (ThresholdFalling < currentValue && currentValue < ThresholdRising) {
 					// Otherwise, if the value is between the falling and rising thresholds, we are in the dead band.
 					// We should not send a message, but just inform the user about the current state.
-				} else if (ThresholdFalling < currentValue && currentValue < ThresholdRising) {
-					node.status({
-						fill: 'green',
-						shape: 'dot',
-						text: `${ThresholdFalling}/${currentValue}/${ThresholdRising} (initial dead band)`
-					});
-
+					setStatus('green', 'dot', 'initial dead band');
+				} else if (currentValue <= ThresholdFalling) {
 					// Finally, if the value is below the falling threshold, we are in the low band, and we should send
 					// the received message.
-				} else if (currentValue <= ThresholdFalling) {
 					newMessage.hysteresis_direction = 'initial low';
-					send(newMessage);
-
 					newDirection = 'low';
 
-					node.status({
-						fill: 'green',
-						shape: 'dot',
-						text: `${ThresholdFalling}/${currentValue}/${ThresholdRising} (initial low band)`
-					});
+					send(newMessage);
+					setStatus('green', 'dot', 'initial low band');
 				}
 			} else if (node.lastValue !== undefined) {
-				// rising
-				if (currentValue > node.lastValue && currentValue >= ThresholdRising && node.direction !== 'high') {
-					newMessage.hysteresis_direction = 'high';
-					send(newMessage);
-
-					newDirection = 'high';
-
-					node.status({
-						fill: 'green',
-						shape: 'dot',
-						text: `${ThresholdFalling}/${currentValue}/${ThresholdRising} (high band)`
-					});
-					// falling
-				} else if (currentValue < node.lastValue && currentValue <= ThresholdFalling && node.direction !== 'low') {
-					newMessage.hysteresis_direction = 'low';
-					send(newMessage);
-
-					newDirection = 'low';
-
-					node.status({
-						fill: 'blue',
-						shape: 'dot',
-						text: `${ThresholdFalling}/${currentValue}/${ThresholdRising} (low band)`
-					});
-				} else if (currentValue > node.lastValue && currentValue >= ThresholdRising && node.direction === 'high') {
-					node.status({
-						fill: 'green',
-						shape: 'dot',
-						text: `${ThresholdFalling}/${currentValue}/${ThresholdRising} (high band rising)`
-					});
-				} else if (currentValue < node.lastValue && currentValue >= ThresholdRising && node.direction === 'high') {
-					node.status({
-						fill: 'green',
-						shape: 'dot',
-						text: `${ThresholdFalling}/${currentValue}/${ThresholdRising} (high band falling)`
-					});
-				} else if (currentValue > node.lastValue && currentValue > ThresholdFalling && currentValue < ThresholdRising && node.direction === 'high') {
-					node.status({
-						fill: 'green',
-						shape: 'ring',
-						text: `${ThresholdFalling}/${currentValue}/${ThresholdRising} (high dead band rising)`
-					});
-				} else if (currentValue < node.lastValue && currentValue > ThresholdFalling && currentValue < ThresholdRising && node.direction === 'high') {
-					node.status({
-						fill: 'green',
-						shape: 'ring',
-						text: `${ThresholdFalling}/${currentValue}/${ThresholdRising} (high dead band falling)`
-					});
-				} else if (currentValue > node.lastValue && currentValue > ThresholdFalling && currentValue < ThresholdRising && node.direction === 'low') {
-					node.status({
-						fill: 'blue',
-						shape: 'ring',
-						text: `${ThresholdFalling}/${currentValue}/${ThresholdRising} (low dead band rising)`
-					});
-				} else if (currentValue < node.lastValue && currentValue > ThresholdFalling && currentValue < ThresholdRising && node.direction === 'low') {
-					node.status({
-						fill: 'blue',
-						shape: 'ring',
-						text: `${ThresholdFalling}/${currentValue}/${ThresholdRising} (low dead band falling)`
-					});
-				} else if (currentValue > node.lastValue && currentValue <= ThresholdFalling && node.direction === 'low') {
-					node.status({
-						fill: 'blue',
-						shape: 'dot',
-						text: `${ThresholdFalling}/${currentValue}/${ThresholdRising} (low band rising)`
-					});
-				} else if (currentValue < node.lastValue && currentValue <= ThresholdFalling && node.direction === 'low') {
-					node.status({
-						fill: 'blue',
-						shape: 'dot',
-						text: `${ThresholdFalling}/${currentValue}/${ThresholdFalling} (low band falling)`
-					});
+				// Rising Value Logic: Determines behavior when the current value is increasing.
+				if (currentValue > node.lastValue) {
+					// High Threshold Crossing: Detects transition into the high state.
+					if (currentValue >= ThresholdRising) {
+						// State Change to High: Triggers when moving from a non-high state to a high state.
+						if (node.direction !== 'high') {
+							// Action: Signal high state, update status, and notify.
+							newMessage.hysteresis_direction = 'high';
+							newDirection = 'high';
+							send(newMessage);
+							setStatus('green', 'dot', 'high band');
+						} else {
+							// Stabilization: Remains in high state without further action.
+							setStatus('green', 'dot', 'high band rising');
+						}
+					}
+					// Dead Band Logic (Rising): Handles values between thresholds, stabilizing output.
+					else if (currentValue > ThresholdFalling && currentValue < ThresholdRising) {
+						if (node.direction === 'high') {
+							setStatus('green', 'ring', 'high dead band rising');
+						} else if (node.direction === 'low') {
+							setStatus('blue', 'ring', 'low dead band rising');
+						}
+					}
+					// Low Band Stabilization: Maintains low state, preventing frequent state toggling.
+					else if (currentValue <= ThresholdFalling && node.direction === 'low') {
+						setStatus('blue', 'dot', 'low band rising');
+					}
+				}
+				// Falling Value Logic: Determines behavior when the current value is decreasing.
+				else if (currentValue < node.lastValue) {
+					// Low Threshold Crossing: Detects transition into the low state.
+					if (currentValue <= ThresholdFalling) {
+						// State Change to Low: Triggers when moving from a non-low state to a low state.
+						if (node.direction !== 'low') {
+							// Action: Signal low state, update status, and notify.
+							newMessage.hysteresis_direction = 'low';
+							newDirection = 'low';
+							send(newMessage);
+							setStatus('blue', 'dot', 'low band');
+						} else {
+							// Stabilization: Remains in low state without further action.
+							setStatus('blue', 'dot', 'low band falling');
+						}
+					}
+					// Dead Band Logic (Falling): Manages values within thresholds, ensuring stability.
+					else if (currentValue > ThresholdFalling && currentValue < ThresholdRising) {
+						if (node.direction === 'high') {
+							setStatus('green', 'ring', 'high dead band falling');
+						} else if (node.direction === 'low') {
+							setStatus('blue', 'ring', 'low dead band falling');
+						}
+					}
+					// High Band Stabilization: Maintains high state, preventing frequent state changes.
+					else if (currentValue >= ThresholdRising && node.direction === 'high') {
+						setStatus('green', 'dot', 'high band falling');
+					}
 				}
 			}
 
@@ -186,9 +164,7 @@ module.exports = function (RED) {
 			node.lastValue = currentValue;
 			nodeContext.set('lastValue', node.lastValue);
 
-			if (done) {
-				done();
-			}
+			done();
 		});
 	}
 
